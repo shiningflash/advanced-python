f2a60200d6d74bd6d4ab42f213ed859bb52444df
amirulislamalmamun@gmail.com <amirulislamalmamun@gmail.com>
2021-07-12 01:40:29 +0600

    sort-string added

---
+++ sort_string.py
@@ -0,0 +1,10 @@
+def sort_words(input):
+    words = input.split()
+    words = [w.lower() + w for w in words]
+    words.sort()
+    words = [w[len(w)//2:] for w in words]
+    return ' '.join(words)
+
+if __name__=='__main__':
+    input = 'one TWO Three FOur'
+    print(sort_words(input))
